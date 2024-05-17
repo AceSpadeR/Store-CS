01a35552ec0514c090c82c39134ee50281746949
Austin Spader <132852502+AceSpadeR@users.noreply.github.com>
2024-05-17 14:26:31 -0600

    Added manufacturer tab

--- DataAccess/ApplicationDbContext.cs
+++ DataAccess/ApplicationDbContext.cs
@@ -11,6 +11,7 @@ namespace DataAccess
 
         }
         public DbSet<Category> Categories { get; set; }  //the physical DB table will be name Categories
+        public DbSet<Manufacturer> Manufacturers { get; set; }
 
 
         //inserting seed data when Model is physically created in the DB the first time
@@ -21,7 +22,17 @@ namespace DataAccess
                 new Category { Id = 2, Name = "Wine", DisplayOrder = 2 },
                 new Category { Id = 3, Name = "Snacks", DisplayOrder = 3 }
                );
+
+            modelBuilder.Entity<Manufacturer>().HasData(
+                new Manufacturer { Id = 1, Name = "Coca Cola"},
+                new Manufacturer { Id = 2, Name = "Yellow Tail"},
+                new Manufacturer { Id = 3, Name = "Trinchero Family Estates" },
+                new Manufacturer { Id = 4, Name = "Doritos"},
+                new Manufacturer { Id = 5, Name = "Cheetos" }
+                );
         }
+
+
 
     }
 
