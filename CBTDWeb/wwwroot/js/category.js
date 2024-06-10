1ae8434a1da6ddc21e0f2c095382683849493386
Austin Spader <132852502+AceSpadeR@users.noreply.github.com>
2024-06-10 14:00:37 -0600

    added the manufacturer and Category data table

---
+++ CBTDWeb/wwwroot/js/category.js
@@ -0,0 +1,38 @@
+﻿
+$(document).ready(function () {
+    loadList();
+});
+
+var dataTable;
+
+function loadList() {
+    dataTable = $('#DT_Categories').DataTable({
+        "ajax": {
+            "url": "/api/category",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "25%" },
+            { "data": "displayOrder", render: $.fn.dataTable.render.number(',', '.', 2, '$'), "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center"> 
+                                <a href="/Categories/Upsert?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width: 100px;">
+                                    <i class="far fa-edit"></i> Edit </a>
+                                <a href="/Categories/Delete?id=${data}" class="btn btn-danger text-white" style="cursor:pointer; width: 100px;">
+                                    <i class="far fa-trash-alt"></i> Delete </a>   
+                            </div>`;
+                }
+                ,
+                "width": "30%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No data found."
+        },
+        "width": "100%",
+        "order": [[2, "asc"]]
+    });
+}
---
+++ CBTDWeb/wwwroot/js/manufacturer.js
@@ -0,0 +1,37 @@
+﻿
+$(document).ready(function () {
+    loadList();
+});
+
+var dataTable;
+
+function loadList() {
+    dataTable = $('#DT_Manufacturers').DataTable({
+        "ajax": {
+            "url": "/api/manufacturer",
+            "type": "GET",
+            "datatype": "json"
+        },
+        "columns": [
+            { "data": "name", "width": "50%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                    return `<div class="text-center"> 
+                                <a href="/Manufacturers/Upsert?id=${data}" class="btn btn-success text-white" style="cursor:pointer; width: 100px;">
+                                    <i class="far fa-edit"></i> Edit </a>
+                                <a href="/Manufacturers/Delete?id=${data}" class="btn btn-danger text-white" style="cursor:pointer; width: 100px;">
+                                    <i class="far fa-trash-alt"></i> Delete </a>   
+                            </div>`;
+                }
+                ,
+                "width": "50%"
+            }
+        ],
+        "language": {
+            "emptyTable": "No data found."
+        },
+        "width": "100%",
+        "order": [[2, "asc"]]
+    });
+}
